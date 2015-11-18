0f0e71a131054ef355b2e7eb9427f8845b2e8880
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2015-11-18 15:32:42 -0500

    framework

---
+++ www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('dashboardCtrl', function($scope) {
+
+})
+   
+.controller('historyCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+      
+.controller('newSessionCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('shoppingCtrl', function($scope) {
+
+})
+   
+.controller('itemDetailCtrl', function($scope) {
+
+})
+   
+.controller('cartCtrl', function($scope) {
+
+})
+   
+.controller('checkoutCtrl', function($scope) {
+
+})
+ 
